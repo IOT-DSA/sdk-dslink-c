48cfa625f069733af2437622bd89a5c417102e0f
samrg472 <samrg472@gmail.com>
2016-01-26 22:52:00 -0800

    Suppress formatting warnings in cmocka

--- test/cmocka.cmake
+++ test/cmocka.cmake
@@ -17,6 +17,7 @@ ExternalProject_Add(cmocka_ep
         -DWITH_STATIC_LIB=ON
         -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_DEBUG:PATH=Debug
         -DCMAKE_ARCHIVE_OUTPUT_DIRECTORY_RELEASE:PATH=Release
+        -DCMAKE_C_ARGS=-Wno-format
 
         # Don't build unused examples and disable install step
         BUILD_COMMAND $(MAKE) cmocka_static
