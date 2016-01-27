a661d9a4a0fa1d43e7c1089fa0293b91462e514c
Kenneth Endfinger <kaendfinger@gmail.com>
2016-01-27 00:39:39 -0500

    Initial Testing Support

---
+++ test/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /Users/alex/DSA/CSDK/test
+# Build directory: /Users/alex/DSA/CSDK/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(col_map_test "col_map_test")
---
+++ test/src/col_map_test.c
@@ -0,0 +1,144 @@
+#include <stddef.h>
+#include <setjmp.h>
+#include <stdarg.h>
+#include <cmocka.h>
+#include <stdlib.h>
+#include <dslink/col/map.h>
+#include <stdio.h>
+
+static
+void col_map_set_simple_string_test(void **state) {
+    (void) state;
+    char *inputs[][2] = {
+            {"A", "Hello World"},
+            {"B", "Goodbye World"},
+            {"C", "World Hello"},
+            {"D", "World Goodbye"},
+            {NULL, NULL}
+    };
+
+    Map map;
+    assert_true(!dslink_map_init(&map, dslink_map_str_cmp,
+                                 dslink_map_str_key_len_cal));
+    int i = 0;
+    while (inputs[i] && inputs[i][0] && inputs[i][1]) {
+        char *key = inputs[i][0];
+        char *val = inputs[i][1];
+
+        void *tmp = val;
+        assert_true(!dslink_map_set(&map, key, &tmp));
+        assert_true(dslink_map_contains(&map, key));
+        assert_null(tmp);
+        char *stored = dslink_map_get(&map, key);
+        assert_non_null(stored);
+        assert_string_equal(stored, val);
+
+        i++;
+    }
+
+    DSLINK_MAP_FREE(&map, {});
+}
+
+static
+void col_map_set_large_string_entry_test(void **state) {
+    (void) state;
+    Map map;
+    assert_true(!dslink_map_init(&map, dslink_map_str_cmp,
+                                 dslink_map_str_key_len_cal));
+    for (int n = 0; n < 9000; n++) {
+        int *i = calloc(1, sizeof(int));
+        *i = n;
+        char* key = calloc(1, sizeof(char) * 6 + 1);
+        char* val = calloc(1, sizeof(char) * 12 + 1);
+        assert_true(sprintf(key, "%i", n));
+        assert_true(sprintf(val, "%i %i", n, n));
+
+        void *tmp = val;
+
+        assert_true(!dslink_map_set(&map, key, &tmp));
+        assert_true(dslink_map_contains(&map, key));
+        assert_null(tmp);
+        char *stored = dslink_map_get(&map, key);
+        assert_non_null(stored);
+        assert_string_equal(stored, val);
+    }
+
+    DSLINK_MAP_FREE(&map, {
+        free(entry->key);
+        free(entry->value);
+    });
+}
+
+static
+void col_map_set_simple_uint32_test(void **state) {
+    (void) state;
+    uint32_t inputs[][2] = {
+            {120, 120},
+            {250, 201},
+            {583, 299},
+            {109, 3982},
+            {0, 0}
+    };
+
+    Map map;
+    assert_true(!dslink_map_init(&map, dslink_map_uint32_cmp,
+                                 dslink_map_uint32_key_len_cal));
+    int i = 0;
+    while (inputs[i] && inputs[i][0] && inputs[i][1]) {
+        uint32_t *key = calloc(1, sizeof(uint32_t));
+        *key = inputs[i][0];
+        uint32_t *val = calloc(1, sizeof(uint32_t));
+        *val = inputs[i][1];
+
+        void *tmp = val;
+        assert_true(!dslink_map_set(&map, key, &tmp));
+        assert_true(dslink_map_contains(&map, key));
+        assert_null(tmp);
+        uint32_t *stored = dslink_map_get(&map, key);
+        assert_non_null(stored);
+        assert_int_equal(stored, val);
+
+        i++;
+    }
+
+    DSLINK_MAP_FREE(&map, {});
+}
+
+static
+void col_map_set_large_uint32_entry_test(void **state) {
+    (void) state;
+    Map map;
+    assert_true(!dslink_map_init(&map, dslink_map_uint32_cmp,
+                                 dslink_map_uint32_key_len_cal));
+    for (int n = 0; n < 9000; n++) {
+        uint32_t *i = calloc(1, sizeof(uint32_t));
+        *i = (uint32_t) n;
+
+        uint32_t *val = calloc(1, sizeof(uint32_t));
+        *val = (uint32_t) (n * 2);
+
+        void *tmp = val;
+        assert_true(!dslink_map_set(&map, i, &tmp));
+        assert_true(dslink_map_contains(&map, i));
+        assert_null(tmp);
+        uint32_t *stored = dslink_map_get(&map, i);
+        assert_non_null(stored);
+        assert_int_equal(stored, val);
+    }
+
+    DSLINK_MAP_FREE(&map, {
+        free(entry->key);
+        free(entry->value);
+    });
+}
+
+int main(void) {
+    const struct CMUnitTest tests[] = {
+            cmocka_unit_test(col_map_set_simple_string_test),
+            cmocka_unit_test(col_map_set_large_string_entry_test),
+            cmocka_unit_test(col_map_set_simple_uint32_test),
+            cmocka_unit_test(col_map_set_large_uint32_entry_test)
+    };
+
+    return cmocka_run_group_tests(tests, NULL, NULL);
+}
