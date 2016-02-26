23e0db4ed58726fe1bccdf266ce75134410ff21a
samrg472 <samrg472@gmail.com>
2016-02-26 13:46:33 -0800

    Add more tests for str replace all

--- test/sdk/utils_test.c
+++ test/sdk/utils_test.c
@@ -6,10 +6,16 @@ static
 void utils_str_replace_all_test(void **state) {
     (void) state;
 
-    char *str = "abc_abc_a";
+    const char *str = "abc_abc_a";
     char *rep = dslink_str_replace_all(str, "a", "123");
     assert_non_null(rep);
     assert_string_equal(rep, "123bc_123bc_123");
+    dslink_free(rep);
+
+    str = "abc_abc";
+    rep = dslink_str_replace_all(str, "abc", "1");
+    assert_non_null(rep);
+    assert_string_equal(rep, "1_1");
     dslink_free(rep);
 }
 
