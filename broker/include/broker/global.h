76cb2ef4b0143f8d2c08a0b579c2b80438dde703
Kenneth Endfinger <k.endfinger@dglogik.com>
2016-03-15 18:52:36 -0400

    Add 'Server Build' metric.

---
+++ broker/include/broker/global.h
@@ -0,0 +1,8 @@
+#ifndef BROKER_GLOBAL_H
+#define BROKER_GLOBAL_H
+
+#ifndef BROKER_SERVER_BUILD
+#define BROKER_SERVER_BUILD "Unknown"
+#endif
+
+#endif //BROKER_GLOBAL_H
--- broker/src/sys/sys.c
+++ broker/src/sys/sys.c
@@ -3,6 +3,33 @@
 #include "broker/sys/token.h"
 #include "broker/sys/restart.h"
 #include "broker/query/query.h"
+#include "broker/global.h"
+
+int init_sys_static(BrokerNode *sysNode) {
+    BrokerNode *buildNode = broker_node_create("build_number", "node");
+    if (!buildNode) {
+        return 1;
+    }
+
+    if (broker_node_add(sysNode, buildNode) != 0) {
+        broker_node_free(buildNode);
+        return 1;
+    }
+
+    if (json_object_set_new(buildNode->meta, "$type",
+                            json_string("string")) != 0) {
+        return 1;
+    }
+
+    if (json_object_set_new(buildNode->meta, "$name",
+                            json_string("Server Build")) != 0) {
+        return 1;
+    }
+
+    buildNode->value = json_string(BROKER_SERVER_BUILD);
+
+    return 0;
+}
 
 int broker_sys_node_populate(BrokerNode *sysNode) {
     if (!sysNode) {
@@ -13,5 +40,6 @@ int broker_sys_node_populate(BrokerNode *sysNode) {
     init_tokens(sysNode);
     init_restart(sysNode);
     init_sys_upstream_node(sysNode);
+    init_sys_static(sysNode);
     return 0;
 }
