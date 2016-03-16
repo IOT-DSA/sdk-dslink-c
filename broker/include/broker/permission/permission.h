0e0c46c737ae86cce1324a3126d32361079273a8
rinick <rinick@gmail.com>
2016-03-16 08:15:57 +0000

    implement permission list check

--- broker/include/broker/permission/permission.h
+++ broker/include/broker/permission/permission.h
@@ -10,14 +10,44 @@ extern "C" {
 #endif
 
 #include <dslink/col/list.h>
+#include <dslink/col/map.h>
+
+struct RemoteDSLink;
+struct BrokerNode;
+struct json_t;
+
+typedef enum {PERMISSION_NONE = 0,
+    PERMISSION_LIST = 10,
+    PERMISSION_READ = 20,
+    PERMISSION_WRITE = 30,
+    PERMISSION_CONFIG = 40,
+    PERMISSION_NEVER = 127} PermissionLevel;
+
+// a list of requester permission groups
+typedef struct PermissionGroups {
+    const char **groups;
+    size_t groupLen;
+} PermissionGroups;
 
 typedef struct PermissionPair {
     char *group;
-    int permission;
+    PermissionLevel permission;
 } PermissionPair;
 
+// virtual permission node for downstream nodes
+typedef struct VirtualPermissionNode {
+    List *permissionList;
+    Map childrenNode;
+} VirtualPermissionNode;
 
-int get_permission(List* permissionList, char **groups);
+void permission_groups_init(PermissionGroups* groups);
+void permission_groups_free(PermissionGroups* groups);
+
+void virtual_permission_init(VirtualPermissionNode* node);
+void virtual_permission_free(VirtualPermissionNode* node);
+void virtual_permission_free_map(Map* map);
+
+PermissionLevel get_permission(const char* path, struct BrokerNode* rootNode, struct RemoteDSLink *reqLink);
 
 #ifdef __cplusplus
 }
