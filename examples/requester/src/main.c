4080cdee004a93547eba4ecb6eeb03a4d95b0aab
Kenneth Endfinger <k.endfinger@dglogik.com>
2016-03-23 11:58:47 -0700

    Separate requester code out into it's own example.

---
+++ examples/requester/src/main.c
@@ -0,0 +1,70 @@
+#define LOG_TAG "main"
+
+#include <dslink/log.h>
+#include <dslink/requester.h>
+
+void on_req_new_val(struct DSLink *link, json_t *resp) {
+    (void) link;
+    printf("Got response %s\n", json_dumps(resp, JSON_INDENT(2)));
+    dslink_requester_close(link, (uint32_t) json_integer_value(json_object_get(resp, "rid")));
+}
+
+void on_val_sub(struct DSLink *link, uint32_t sid, json_t *val, json_t *ts) {
+    (void) link;
+    (void) ts;
+    (void) sid;
+    printf("Got value %f\n", json_real_value(val));
+    dslink_requester_unsubscribe(link, sid);
+}
+
+void on_req_close(struct DSLink *link, json_t *resp) {
+    (void) link;
+    (void) resp;
+    json_t *rid = json_object_get(resp, "rid");
+    printf("Request %i closed.\n", (int) json_integer_value(rid));
+}
+
+void init(DSLink *link) {
+    (void) link;
+    log_info("Initialized!\n");
+}
+
+void connected(DSLink *link) {
+    (void) link;
+    log_info("Connected!\n");
+}
+
+void disconnected(DSLink *link) {
+    (void) link;
+    log_info("Disconnected!\n");
+}
+
+void requester_ready(DSLink *link) {
+    ref_t *refa = dslink_requester_list(link, "/downstream", on_req_new_val);
+    ref_t *refb = dslink_requester_subscribe(link, "/downstream/System/CPU_Usage", on_val_sub);
+
+    {
+        RequestHolder *req = refa->data;
+        req->close_cb = on_req_close;
+    }
+
+    {
+        RequestHolder *req = refb->data;
+        req->close_cb = on_req_close;
+    }
+}
+
+void configure_request(ref_t *ref) {
+    RequestHolder *req = ref->data;
+}
+
+int main(int argc, char **argv) {
+    DSLinkCallbacks cbs = {
+        init,
+        connected,
+        disconnected,
+        requester_ready
+    };
+
+    return dslink_init(argc, argv, "C-Requester", 1, 0, &cbs);
+}
--- examples/responder/src/main.c
+++ examples/responder/src/main.c
@@ -1,30 +1,9 @@
 #define LOG_TAG "main"
 
 #include <dslink/log.h>
-#include <dslink/requester.h>
 #include "replicator.h"
 #include "rng.h"
 #include "invoke.h"
-
-void on_req_new_val(struct DSLink *link, json_t *resp) {
-    (void) link;
-    printf("Got response %s\n", json_dumps(resp, JSON_INDENT(2)));
-    dslink_requester_close(link, (uint32_t) json_integer_value(json_object_get(resp, "rid")));
-}
-
-void on_val_sub(struct DSLink *link, uint32_t sid, json_t *val, json_t *ts) {
-    (void) link;
-    (void) ts;
-    (void) sid;
-    printf("Got value %f\n", json_real_value(val));
-    dslink_requester_unsubscribe(link, sid);
-}
-
-void on_req_close(struct DSLink *link, json_t *resp) {
-    (void) link;
-    (void) resp;
-    printf("Request closed.\n");
-}
 
 void init(DSLink *link) {
     DSNode *superRoot = link->responder->super_root;
@@ -46,28 +25,13 @@ void disconnected(DSLink *link) {
     log_info("Disconnected!\n");
 }
 
-void requester_ready(DSLink *link) {
-    ref_t *refa = dslink_requester_list(link, "/downstream", on_req_new_val);
-    ref_t *refb = dslink_requester_subscribe(link, "/downstream/System/CPU_Usage", on_val_sub);
-
-    {
-        RequestHolder *req = refa->data;
-        req->close_cb = on_req_close;
-    }
-
-    {
-        RequestHolder *req = refb->data;
-        req->close_cb = on_req_close;
-    }
-}
-
 int main(int argc, char **argv) {
     DSLinkCallbacks cbs = {
         init,
         connected,
         disconnected,
-        requester_ready
+        NULL
     };
 
-    return dslink_init(argc, argv, "C-Resp", 1, 1, &cbs);
+    return dslink_init(argc, argv, "C-Resp", 0, 1, &cbs);
 }
