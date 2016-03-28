da25e53e1024fefa3767888ab6e405b1835445a7
Kenneth Endfinger <k.endfinger@dglogik.com>
2016-03-27 23:54:00 -0400

    Actually ensure an out of source build.

--- tools/cmake/MacroEnsureOutOfSourceBuild.cmake
+++ tools/cmake/MacroEnsureOutOfSourceBuild.cmake
@@ -1,17 +1,16 @@
-# - MACRO_ENSURE_OUT_OF_SOURCE_BUILD(<errorMessage>)
-# MACRO_ENSURE_OUT_OF_SOURCE_BUILD(<errorMessage>)
+# - ensure_out_of_source_build(<errorMessage>)
+# ensure_out_of_source_build(<errorMessage>)
 
 # Copyright (c) 2006, Alexander Neundorf, <neundorf@kde.org>
 #
 # Redistribution and use is allowed according to the terms of the BSD license.
 # For details see the accompanying COPYING-CMAKE-SCRIPTS file.
 
-macro (MACRO_ENSURE_OUT_OF_SOURCE_BUILD _errorMessage)
+macro (ensure_out_of_source_build _errorMessage)
 
-    string(COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" _insource)
-    if (_insource)
+    if ("${CMAKE_SOURCE_DIR}" STREQUAL "${CMAKE_BINARY_DIR}")
         message(SEND_ERROR "${_errorMessage}")
         message(FATAL_ERROR "Remove the file CMakeCache.txt in ${CMAKE_SOURCE_DIR} first.")
-    endif (_insource)
+    endif()
 
-endmacro (MACRO_ENSURE_OUT_OF_SOURCE_BUILD)
+endmacro (ensure_out_of_source_build)
