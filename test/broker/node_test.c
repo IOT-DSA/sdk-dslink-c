aa9c389f87067cc7982bf58771212b8214bef9bd
Kenneth Endfinger <k.endfinger@dglogik.com>
2016-04-12 19:02:12 -0400

    Fix the broker node tests.

--- test/broker/node_test.c
+++ test/broker/node_test.c
@@ -139,7 +139,6 @@ void node_get_test(void **state) {
         assert_non_null(n);
         n->type = DOWNSTREAM_NODE;
         n->name = dslink_strdup("test");
-        assert_true(!dslink_map_init(&n->list_streams, NULL, NULL, NULL));
 
         assert_non_null(n->name);
         assert_true(!broker_node_add(d, (BrokerNode *) n));
