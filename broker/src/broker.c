2364239df1c2d3f65bb8a30631c0dba727dc1d97
Norbert Heusser <nheusser@cisco.com>
2017-04-06 23:58:57 +0200

    Renamed git commit hash define to IOT_DSA_C_SDK_GIT_COMMIT_HASH and set as global compiler define

--- broker/src/broker.c
+++ broker/src/broker.c
@@ -25,8 +25,8 @@
                     "Content-Length: %d\r\n" \
                     "\r\n%s\r\n"
 
-#ifndef C_SDK_GIT_COMMIT_HASH
-#define C_SDK_GIT_COMMIT_HASH "unknown"
+#ifndef IOT_DSA_C_SDK_GIT_COMMIT_HASH
+#define IOT_DSA_C_SDK_GIT_COMMIT_HASH "unknown"
 #endif
 
 uv_loop_t *mainLoop = NULL;
@@ -395,7 +395,7 @@ void broker_stop(Broker* broker) {
 }
 
 int broker_start() {
-    log_info("C-SDK git commit: %s\n", C_SDK_GIT_COMMIT_HASH);
+    log_info("IOT-DSA c-sdk git commit: %s\n", IOT_DSA_C_SDK_GIT_COMMIT_HASH);
 
     int ret = 0;
     json_t *config = broker_config_get();
