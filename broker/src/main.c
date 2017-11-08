1f6505a17ff7e3b22875f6eb1063e03b1ba8b72a
Lars Fuerstenberg (lfuerste) <lfuerste@cisco.com>
2017-11-08 12:31:12 +0100

    EAF-591 added command line option --version to the broker

--- broker/src/main.c
+++ broker/src/main.c
@@ -1,5 +1,9 @@
 #include "broker/broker.h"
 
-int main() {
+int main(int argc, char** argv) {
+    if(argc == 2 && strcmp("--version", argv[1]) == 0) {
+        printf("IOT-DSA c-broker version: %s\n", IOT_DSA_C_SDK_VERSION);
+        return 0;
+    }
     return broker_start();
 }
